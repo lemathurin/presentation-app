0ccd5dc558a3a9e3a1fe05d0fd684e475a19761f
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-04-22 18:18:12 +0200

    added theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+
 function App() {
   return (
     <div>
-      <h1 className="text-orange-300">Welcome to Tauri!</h1>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1 className="text-orange-300">Welcome to Tauri!</h1>
+        <ModeToggle />
+      </ThemeProvider>
     </div>
   );
 }
