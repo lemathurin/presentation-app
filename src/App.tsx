e9afd35318a87e5fdee1ff9a7989b3d9e6a2994a
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-04-22 19:15:52 +0200

    added base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 function App() {
   return (
-    <div>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <h1 className="text-orange-300">Welcome to Tauri!</h1>
-        <ModeToggle />
-      </ThemeProvider>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="bg-orange-300 w-screen h-screen">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="max-w-full border"
+        >
+          <ResizablePanel defaultSize={25} minSize={20}>
+            {/* add this later collapsible={true} */}
+            <div className="flex items-center justify-center">
+              <span className="font-semibold">One</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel defaultSize={75} minSize={40}>
+            <div className="flex items-center justify-center">
+              <span className="font-semibold">Two</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </ThemeProvider>
   );
 }
 
