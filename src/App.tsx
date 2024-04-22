6955d78df88afc897fe1993ee7d33419e45475cc
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-04-22 22:42:59 +0200

    added top bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,27 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { TopBar } from "./components/top-bar";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="bg-orange-300 w-screen h-screen">
+      <div className="w-screen h-screen">
         <ResizablePanelGroup
           direction="horizontal"
           className="max-w-full border"
         >
-          <ResizablePanel defaultSize={25} minSize={20}>
+          <ResizablePanel defaultSize={25} minSize={20} maxSize={40}>
             {/* add this later collapsible={true} */}
-            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-center h-full bg-secondary">
               <span className="font-semibold">One</span>
+              <ModeToggle />
             </div>
           </ResizablePanel>
           <ResizableHandle />
-          <ResizablePanel defaultSize={75} minSize={40}>
-            <div className="flex items-center justify-center">
+          <ResizablePanel defaultSize={75}>
+            <TopBar />
+            <div className="flex items-center justify-center h-full bg-primary-foreground">
               <span className="font-semibold">Two</span>
             </div>
           </ResizablePanel>
---
+++ src/components/top-bar.tsx
@@ -0,0 +1,7 @@
+export function TopBar() {
+  return (
+    <div className="w-full h-12 border-b bg-primary-foreground">
+      <h1>Top Bar</h1>
+    </div>
+  );
+}
