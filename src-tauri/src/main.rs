7ab71f7c45fafd091bfb5feb425dbd45754e88a4
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-05-28 14:41:14 +0200

    new initial window - main window still doesn't open

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,30 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
+// fn main() {
+//     tauri::Builder::default()
+//         .invoke_handler(tauri::generate_handler![greet])
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
+
+use tauri::{Manager, WindowBuilder};
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![open_main_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn open_main_window(app: tauri::AppHandle) {
+    let main_window = app.get_window("main").unwrap(); // Use "main" instead of "index"
+    main_window.show().unwrap();
+    app.get_window("initial").unwrap().close().unwrap();
 }
