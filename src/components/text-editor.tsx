f9eeee5f9ad953a7feebee470930c73ea657f491
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-06-04 12:36:28 +0200

    basic md editor installed

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MDEditor from "@uiw/react-md-editor";
 
 interface TextEditorProps {
   text: string;
@@ -6,17 +7,26 @@ interface TextEditorProps {
 }
 
 const TextEditor: React.FC<TextEditorProps> = ({ text, setText }) => {
+  // const [value, setValue] = React.useState("**Hello world!!!**");
+
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setText(e.target.value);
   };
 
   return (
-    <textarea
-      className="w-[100%] h-[100%] p-3 bg-background"
-      value={text}
-      onChange={handleChange}
-      placeholder="Type something..."
-    />
+    // <textarea
+    //   className="w-[100%] h-[100%] p-3 bg-background"
+    //   value={text}
+    //   onChange={handleChange}
+    //   placeholder="Type something..."
+    // />
+    <div className="container">
+      <MDEditor
+        value={text}
+        onChange={(handleChange) => setText(handleChange || "")}
+      />
+      <MDEditor.Markdown source={text} style={{ whiteSpace: "pre-wrap" }} />
+    </div>
   );
 };
 
